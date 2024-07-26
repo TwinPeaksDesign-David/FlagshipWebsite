e45ac7625a3242b36d339e806c24c50801f54a15
TwinPeaksWebDesign-David <tpwd.info@gmail.com>
2024-07-26 15:13:31 -0700

    make flagship site

---
+++ data/navdata.json.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        text : "Home",
+        href : "/",
+    },
+    {
+        text : "About Us",
+        href : "/about/",
+    },
+    {
+        text : "Services",
+        href : "/services/",
+    },
+    {
+        text : "Contact Us",
+        href : "/contact/",
+    },
+]
